3b75333a29e2523d3200425d7ca2bbb870e15a5e
Michael Fluharty <michael.fluharty@mac.com>
2025-10-11 12:02:35 -0500

    Initial Commit

---
+++ NGWebPortalTests/NGWebPortalTests.swift
@@ -0,0 +1,17 @@
+//
+//  NGWebPortalTests.swift
+//  NGWebPortalTests
+//
+//  Created by Michael Fluharty on 10/11/25.
+//
+
+import Testing
+@testable import NGWebPortal
+
+struct NGWebPortalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
